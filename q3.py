047112bf0644594d2aa1f89d211c144ddc4374aa
edwinyapweitian <46862873+edwinyapweitian@users.noreply.github.com>
2025-12-19 18:27:51 +0800

    Implement update_dictionary function
    Add update_dictionary function to modify dictionary values.

--- q3.py
+++ q3.py
@@ -7,8 +7,23 @@ def update_dictionary(dct, key, value):
     """
     return
 
+def update_dictionary(dct, key, value):
+
+    if not isinstance(dct, dict):
+        return -1
+    if key in dct:
+        print(dct[key])
+    dct[key] = value
+    return dct
 
 # Task 2
 # Invoke the function "update_dictionary" using the following scenarios:
+
 # - {}, "name", "Alice"
+   update_dictionary({}, "name", "Alice")
+    -> returns: {'name': 'Alice'} (no printed original value)
+
 # - {"age": 25}, "age", 26
+    update_dictionary({"age": 25}, "age", 26)
+    -> prints: 25
+    -> returns: {'age': 26}
